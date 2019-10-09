b9a56ceb43a50875fb3c45f6ec0c7df9f83d2e1b
belja <belja@umich.edu>
2019-10-09 13:10:17 -0400

    Updating to our server

--- Library/Utilities/ServerConfig.cs
+++ Library/Utilities/ServerConfig.cs
@@ -12,7 +12,7 @@
         /// Example of ws: websockets over http
         /// AWS (elastic beanstalk) default
         /// </remarks>
-        public static readonly string AwsAddress = "ws://apollosignaller-env.p47zti3ztv.us-east-2.elasticbeanstalk.com/";
+        public static readonly string AwsAddress = "ws://Theialens.ym3ycmfhxt.us-east-2.elasticbeanstalk.com/";
 
         /// <summary>
         /// NO LONGER ACTIVE: EXAMPLE ONLY
