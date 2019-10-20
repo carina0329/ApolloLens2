7e55089ec23181f78b847ebcf3373be6df18b701
Jimil Patel <pateljim@umich.edu>
2019-10-20 16:58:10 -0400

    [master] added standalone vitals app. This uses TCP to establish a connection with the ApolloLensVitalsSource python server and will constantly show the updated vitals values

---
+++ ApolloLensVitals/MainPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+
+
+namespace JsonExample.Serialization
+{
+    public interface ISerializer
+    {
+        string Serialize<TEntity>(TEntity entity)
+            where TEntity : class, new();
+
+        TEntity Deserialize<TEntity>(string entity)
+            where TEntity : class, new();
+    }
+}
+
+namespace JsonExample.Serialization
+{
+    public class JsonSerializer : ISerializer
+    {
+        public string Serialize<TEntity>(TEntity entity)
+            where TEntity : class, new()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(TEntity));
+                ser.WriteObject(ms, entity);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        public TEntity Deserialize<TEntity>(string entity)
+            where TEntity : class, new()
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(TEntity));
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(entity)))
+            {
+                return ser.ReadObject(stream) as TEntity;
+            }
+        }
+    }
+}
+
+
+
+
+
+namespace ApolloLensVitals
+{
+    /// <summary>
+    /// Vitals Main Page.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private VitalsConnect vitals;
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs args)
+        {
+            System.Diagnostics.Debug.WriteLine("Start");
+            this.vitals = new VitalsConnect(ChangeText);
+            this.vitals.Run();
+            System.Diagnostics.Debug.WriteLine("Made it past run call.");
+        }
+
+        public delegate void ChangeTextDelegate(string bp);
+
+
+        public async void ChangeText(string bp)
+        {
+            /////// var stream1 = new MemoryStream();
+            // var ser = new DataContractJsonSerializer(typeof(Vitals));
+            //stream1.Position = 0;
+            // var p2 = (Vitals)ser.ReadObject(stream1);
+            //Vitals p2 = JsonExample.Serialization.JsonSerializer.Deserialize<Vitals>(bp);
+
+            //// JsonExample.Serialization.JsonSerializer x = new JsonExample.Serialization.JsonSerializer();
+            //       Vitals p2 = x.Deserialize<Vitals>(bp);
+            bp = bp.Replace("\0", string.Empty);
+
+            Vitals p2 = JsonConvert.DeserializeObject<Vitals>(bp);
+
+            
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                this.BloodPressureText.Text = p2.blood_pressure_systolic + " | " + p2.blood_pressure_diastolic;
+                this.HeartRateText.Text = p2.heart_rate;
+                this.RespirationRateText.Text = p2.respiration_rate;
+                //System.Diagnostics.Debug.WriteLine(bp);
+
+
+               //// this.BloodPressureText.Text = bp;
+            //    this.HeartRateText.Text = bp;
+             //   this.RespirationRateText.Text = bp;
+            });
+           // this.BloodPressureText.Text = bp;
+          //  this.UpdateLayout();
+        }
+    }
+
+    //DataContrac
+    public class Vitals
+    {
+        ////[DataMember(Name = "heart_rate", Order = 0)]
+        public string heart_rate { get; set; }
+
+//[DataMember(Name = "blood_pressure_systolic", Order = 1)]
+        public string blood_pressure_systolic { get; set; }
+
+        //DataMember(Name = "blood_pressure_diastolic", Order = 2)]
+        public string blood_pressure_diastolic { get; set; }
+
+//[DataMember(Name = "respiration_rate", Order = 3)]
+        public string respiration_rate { get; set; }
+    }
+}
+
+
---
+++ ApolloLensVitals/VitalsConnect.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ApolloLensVitals
+{
+    /// <summary>
+    /// Encapsulates connection with Vitals Source Server.
+    /// In charge of receiving Vitals data.
+    /// </summary>
+    public class VitalsConnect
+    {
+        private Socket sock;
+        private IPHostEntry ipHost;
+        private IPAddress ipAddr;
+        private IPEndPoint localEndPoint;
+        private TcpClient tc;
+        private MainPage.ChangeTextDelegate changeText;
+        private int byteArrInd;
+
+        /// <summary>
+        /// Constructor. Calls InitClient.
+        /// </summary>
+        public VitalsConnect(MainPage.ChangeTextDelegate c)
+        {
+            this.changeText = c;
+            this.InitClient();
+        }
+
+        public void Run()
+        {
+            ThreadStart childref = new ThreadStart(this.RunThread);
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+        }
+
+        private void RunThread()
+        {
+            try
+            {
+                this.Connect();
+                this.Listen();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                System.Diagnostics.Debug.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+
+        private void Connect()
+        {
+            // connect socket
+            this.sock.Connect(this.localEndPoint);
+           // this.tc = new TcpClient();
+          //  this.tc.Connect(this.ipAddr, 10000);
+            byte[] verifyMessage = Encoding.UTF8.GetBytes("Test");
+            this.sock.Send(verifyMessage);
+        }
+
+        private void Listen()
+        {
+            // listen for vitals from the source
+            byteArrInd = 0;
+            byte[] msgArr = new byte[2048];
+            while (true)
+            {
+                // Data buffer 
+                byte[] recvArr = new byte[1];
+                int byteRecv = this.sock.Receive(recvArr);
+
+                msgArr[byteArrInd] = recvArr[0];
+                if (recvArr[0] == '}')
+                {
+                    
+                    string jsonStr = Encoding.UTF8.GetString(msgArr);
+                    this.changeText(jsonStr);
+                    for (int clear = 0; clear <= byteArrInd; ++clear)
+                    {
+                        msgArr[clear] = default(byte);
+                    }
+                    byteArrInd = 0;
+                    
+                    
+                }
+                else
+                {
+                    ++byteArrInd;
+                }
+
+
+                
+            }
+
+        }
+
+
+        /// <summary>
+        /// Initializes the client by setting up the socket and
+        /// sending bytes to confirm connection (need to change auth)
+        /// </summary>
+        public void InitClient()
+        {
+            try
+            {
+                // determine destination
+                // this.ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                //this.ipHost = Dns.GetHostEntry("localhost");
+                //this.ipAddr = this.ipHost.AddressList[0];
+                // this.ipAddr = new IPAddress(Encoding.ASCII.GetBytes("127.0.0.1"));
+                this.ipAddr = IPAddress.Loopback;
+                this.localEndPoint = new IPEndPoint(this.ipAddr, 10000);
+
+                // create socket
+                this.sock = new Socket(
+                    this.ipAddr.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                );
+                //this.sock.ReceiveTimeout = 2000;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+    }
+}
