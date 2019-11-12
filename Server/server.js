c81750ce1deedb016d259edf34eb5153fc7d0d9c
p4telj <pateljim@umich.edu>
2019-11-12 14:53:12 -0500

    [Jimil] final server change for now

--- Server/server.js
+++ Server/server.js
@@ -14,36 +14,103 @@ const ServerFullCode = 4000;
 const ServerFullError = 'ServerFullError';
 
 // used for logging
-let nextConnectionId = 0;
+let uniqueConnectionId = 0;
+
+// 1 source connection invariant
+let sourceConnections = 0;
+
+// last pinged client to ensure server responds to appropriate client
+// when establishing webrtc connections
+let lastPingedClient = -1;
 
 // handler for a new connection to the server
-wss.on('connection', (ws) => {
+wss.on('connection', function connection(ws, request, client) {
 
     // mark this connection uniquely
-    let connectionId = nextConnectionId;
-    nextConnectionId++;
+    ws.uid = uniqueConnectionId++;
 
-    // rejects connections after the first 2
-    if (wss.clients.size > 2) {
-        console.log(`Rejected connection ${connectionId}`);
-        ws.close(ServerFullCode, ServerFullError);
-    }
-    else {
-        console.log(`Accepted connection ${connectionId}`);
-    }
-    
+    console.log(`Opened connection ${ws.uid}`);
+
     // handler for receiving a message on the socket
-    ws.on('message', (message) => {        
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === ws.OPEN) {
-                client.send(message);
+    ws.on('message', (message) => {
+        // (1) registration uses a plain string
+        // (2) hello world test. converts to JSON first.
+        //  source forwards to all clients
+        //  client only sends to source
+        // (3) webrtc establishment via ice exhcnage, etc..
+        //  client -> source. source -> same client.
+        //  this is where keeping track of who's who matters most to enable one-to-many
+
+
+        // registration / string
+        if (message === "client" || message == "source") {
+            ws.id = message;
+
+            if (ws.id === "source" && sourceConnections === 1) {
+                console.log(`Rejected connection ${ws.id} ${ws.uid}`);
+                ws.close(ServerFullCode, ServerFullError);
             }
-        });
+
+            // increment open source connections
+            console.log(`Registered connection ${ws.uid} as ${ws.id}`);
+            sourceConnections += ws.id === "source";
+        }
+        else {
+
+            let jmsg = JSON.parse(message);
+            let target = ws.id === "client" ? "source" : "client";
+
+            if (jmsg.MessageContents === "Hello, World!") {
+                wss.clients.forEach((client) => {
+                    if (client !== ws && client.id === target && client.readyState === ws.OPEN) {
+                        client.send(message);
+                    }
+                });
+            }
+            else if (jmsg.MessageType == "5") {
+                wss.clients.forEach((client) => {
+                    // target = source. we don't want to send to source
+                    if (client !== ws && client.id != target && client.readyState === ws.OPEN) {
+                        client.send(message);
+                    }
+                })
+            }
+            else if (target === "source") {
+                lastPingedClient = ws.uid;
+                wss.clients.forEach((client) => {
+                    if (client.id === target && client.readyState === ws.OPEN) {
+                        client.send(message);
+                    }
+                });
+            }
+            else {
+                wss.clients.forEach((client) => {
+                    if (client.uid === lastPingedClient && client.readyState === ws.OPEN) {
+                        client.send(message);
+                    }
+                })
+            }
+        }
     });
 
     // handler for the socket connection closing
     ws.on('close', () => {
-        console.log(`Closed connection ${connectionId}`);
+        console.log(`Closed connection ${ws.id} ${ws.uid}`);
+
+        // decrement sourceConnections and drop all clients if source disconnects
+        // source expected to stay connected to signaller throughout the call
+        // to allow other clients to join at any time.
+        if (ws.id === "source") {
+            console.log(`${ws.uid} was a source connection. Dropping all clients.`);
+            --sourceConnections;
+            wss.clients.forEach((client) => {
+                if (client !== ws) {
+                    console.log(`Dropped connection ${client.id} ${client.uid}`);
+                    client.close(ServerFullCode, ServerFullError);
+                }
+            })
+        }
+        
     });
 });
 
