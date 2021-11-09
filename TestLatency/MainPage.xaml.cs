bb7ba32cac5e3dcead5b70533e1a0f5f0db7c8b2
allenh9999 <allenh9999@gmail.com>
2021-11-09 18:13:10 -0500

    Finished testlatency code
    Co-Authored-By: carina0329 <53321343+carina0329@users.noreply.github.com>
    Co-Authored-By: shradhareddy <20015431+shradhareddy@users.noreply.github.com>
    Co-Authored-By: Brandon Garzez <77688749+bgarzez@users.noreply.github.com>

--- TestLatency/MainPage.xaml.cs
+++ TestLatency/MainPage.xaml.cs
@@ -18,6 +18,10 @@ using System.Diagnostics;
 using Windows.Media.Capture;
 using Windows.ApplicationModel;
 using System.Collections.Generic;
+using TestLatency.Video;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Media.MediaProperties;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,6 +33,11 @@ namespace TestLatency
     public sealed partial class MainPage : Page
     {
         private PeerConnection peerConnection;
+        private MediaStreamSource _localVideoSource;
+        private VideoBridge _localVideoBridge = new VideoBridge(3);
+        private bool _localVideoPlaying = false;
+        private object _localVideoLock = new object();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -80,6 +89,8 @@ namespace TestLatency
                 trackName = "microphone_track"
             };
             _localAudioTrack = LocalAudioTrack.CreateFromSource(_microphoneSource, audioTrackConfig);
+            _webcamSource = await DeviceVideoTrackSource.CreateAsync();
+            _webcamSource.I420AVideoFrameReady += LocalI420AFrameReady;
         }
 
         private void App_Suspending(object sender, SuspendingEventArgs e)
@@ -89,6 +100,91 @@ namespace TestLatency
                 peerConnection.Close();
                 peerConnection.Dispose();
                 peerConnection = null;
+                localVideoPlayerElement.SetMediaPlayer(null);
+            }
+        }
+
+        private MediaStreamSource CreateI420VideoStreamSource(
+            uint width, uint height, int framerate)
+        {
+            if (width == 0)
+            {
+                throw new ArgumentException("Invalid zero width for video.", "width");
+            }
+            if (height == 0)
+            {
+                throw new ArgumentException("Invalid zero height for video.", "height");
+            }
+            // Note: IYUV and I420 have same memory layout (though different FOURCC)
+            // https://docs.microsoft.com/en-us/windows/desktop/medfound/video-subtype-guids
+            var videoProperties = VideoEncodingProperties.CreateUncompressed(
+                MediaEncodingSubtypes.Iyuv, width, height);
+            var videoStreamDesc = new VideoStreamDescriptor(videoProperties);
+            videoStreamDesc.EncodingProperties.FrameRate.Numerator = (uint)framerate;
+            videoStreamDesc.EncodingProperties.FrameRate.Denominator = 1;
+            // Bitrate in bits per second : framerate * frame pixel size * I420=12bpp
+            videoStreamDesc.EncodingProperties.Bitrate = ((uint)framerate * width * height * 12);
+            var videoStreamSource = new MediaStreamSource(videoStreamDesc);
+            videoStreamSource.BufferTime = TimeSpan.Zero;
+            videoStreamSource.SampleRequested += OnMediaStreamSourceRequested;
+            videoStreamSource.IsLive = true; // Enables optimizations for live sources
+            videoStreamSource.CanSeek = false; // Cannot seek live WebRTC video stream
+            return videoStreamSource;
+        }
+
+        private void OnMediaStreamSourceRequested(MediaStreamSource sender,
+            MediaStreamSourceSampleRequestedEventArgs args)
+        {
+            VideoBridge videoBridge;
+            if (sender == _localVideoSource)
+                videoBridge = _localVideoBridge;
+            else
+                return;
+            videoBridge.TryServeVideoFrame(args);
+        }
+
+        private void LocalI420AFrameReady(I420AVideoFrame frame)
+        {
+            lock (_localVideoLock)
+            {
+                if (!_localVideoPlaying)
+                {
+                    _localVideoPlaying = true;
+
+                    // Capture the resolution into local variable useable from the lambda below
+                    uint width = frame.width;
+                    uint height = frame.height;
+
+                    // Defer UI-related work to the main UI thread
+                    RunOnMainThread(() =>
+                    {
+                        // Bridge the local video track with the local media player UI
+                        int framerate = 30; // assumed, for lack of an actual value
+                        _localVideoSource = CreateI420VideoStreamSource(
+                            width, height, framerate);
+                        var localVideoPlayer = new MediaPlayer();
+                        localVideoPlayer.Source = MediaSource.CreateFromMediaStreamSource(
+                            _localVideoSource);
+                        localVideoPlayerElement.SetMediaPlayer(localVideoPlayer);
+                        localVideoPlayer.Play();
+                    });
+                }
+            }
+            // Enqueue the incoming frame into the video bridge; the media player will
+            // later dequeue it as soon as it's ready.
+            _localVideoBridge.HandleIncomingVideoFrame(frame);
+        }
+
+        private void RunOnMainThread(Windows.UI.Core.DispatchedHandler handler)
+        {
+            if (Dispatcher.HasThreadAccess)
+            {
+                handler.Invoke();
+            }
+            else
+            {
+                // Note: use a discard "_" to silence CS4014 warning
+                _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, handler);
             }
         }
     }
